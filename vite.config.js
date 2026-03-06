0d5beeb5520a8b1daa58fd69ef243ecfc87d1434
Nick <nsoltysiak@pm.me>
2026-03-06 18:09:59 -0500

    index update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/react-counter-app/",
   plugins: [react()],
+  build: {
+    assetsDir: "assets",
+  },
 });
